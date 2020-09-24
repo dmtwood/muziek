18f8aa95042a37f6690eebcb7c135eb6ca742708
dmtG <dimitri.gevers@gmail.com>
2020-09-24 19:19:22 +0200

    first commit album.java tracks line 26

--- src/main/java/be/vdab/muziek/domain/Album.java
+++ src/main/java/be/vdab/muziek/domain/Album.java
@@ -1,15 +1,29 @@
 package be.vdab.muziek.domain;
 
-import javax.persistence.*;
+import org.hibernate.mapping.Collection;
 
-@Entity
-@Table( name = "albums" )
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.Set;
+
+@Entity @Table( name = "albums" )
 public class Album {
     @Id @GeneratedValue
     private long id;
     private String naam;
 
+
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn( name = "artiestid")
     private Artiest artiest;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "tracks",
+            joinColumns = @JoinColumn( name = "albumid")
+    ) private Set<Track> getTracks(){
+        return Collections.unmodifiableSet(tracks);
+    }
 
 }
--- src/main/java/be/vdab/muziek/domain/Artiest.java
+++ src/main/java/be/vdab/muziek/domain/Artiest.java
@@ -1,19 +1,31 @@
 package be.vdab.muziek.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity @Table( name = "artiest")
 public class Artiest {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String naam;
+
+    @OneToMany
+    private Set<Album> albumSet;
 
     protected Artiest() {
     }
     public Artiest( String naam) {
         this.naam = naam;
+        this.albumSet = new LinkedHashSet<>();
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNaam() {
+        return naam;
     }
 }
